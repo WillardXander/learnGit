34d502d0f0b4a5cff6735ec2f736aa68f9e3407e
wx <1509224490@qq.com>
2022-11-01 22:35:37 +0800

    add test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+/**
+ * @ClassName test
+ * @Author 15092
+ * @Date 2022/11/1
+ * @Version 1.0
+ * @Description
+ **/
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("hello, world");
+    }
+    
+}
